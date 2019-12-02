0e382436cf6359ff21bccc199531826b9bc466a9
Alex <alexrtucker@hotmail.co.uk>
2019-12-02 15:19:24 +0000

    Finished fizzbuzz and added tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,19 @@
-describe 'fizzbuzz' do
-  it 'returns "fizz" when 3 passed' do
-    expect(fizzbuzz(3).to eq 'fizz')
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when 3 passed" do
+    expect(fizzbuzz(3||6||9)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'buzz' when 5 passed" do
+    expect(fizzbuzz(5||10||20)).to eq "buzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it "returns 'fizzbuzz' when 15 passed" do
+    expect(fizzbuzz(15||30)).to eq "fizzbuzz"
   end
 end
